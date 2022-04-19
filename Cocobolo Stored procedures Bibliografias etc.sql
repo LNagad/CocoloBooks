a89b562a419c54a11d68fea3ef20b241047e35bb
Deivi13 <61521077+Deivi13@users.noreply.github.com>
2022-04-19 16:18:25 -0400

    Update Cocobolo Stored procedures Bibliografias etc.sql
    Added new store procedure for a project sp_ValidarUsuario

--- Cocobolo Stored procedures Bibliografias etc.sql
+++ Cocobolo Stored procedures Bibliografias etc.sql
@@ -5,6 +5,19 @@ select * from Bibliografias
 alter table Bibliografias
 ADD Estado bit default 1;
 
+create proc sp_ValidarUsuario(
+@Correo varchar(100),
+@Clave varchar(500)
+)
+as
+begin
+    if(exists(select * from USUARIOS where Correo = @Correo and Clave = @Clave))
+    	select Id from USUARIOS where Correo = @Correo and Clave = @Clave
+    else
+        select '0'
+end
+    
+
 create proc sp_registrarBiibliografia
 (
 @Name varchar(100),
