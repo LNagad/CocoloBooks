a5ed354cf14232b57e3fe17e300a4b61cc1af619
AlanYoel <alanyoelsancheza@gmail.com>
2022-04-24 17:44:30 -0400

    Cambios-layout-empleados

--- CapaDatos/CD_Empleados.cs
+++ CapaDatos/CD_Empleados.cs
@@ -1,12 +1,184 @@
-﻿using System;
+﻿using CapaEntidad;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class CD_Empleados
+    public class CD_Empleados
     {
+        public List<Empleados> Listar()
+        {
+            List<Empleados> lista = new List<Empleados>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select * from Empleados";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader DR = cmd.ExecuteReader())
+                    {
+                        while (DR.Read())
+                        {
+                            lista.Add(new Empleados()
+                            {
+                                Id = Convert.ToInt32(DR["Id"]),
+                                Nombre = DR["Nombre"].ToString(),
+                                Cedula = DR["Cedula"].ToString(),
+                                TandaLabor = DR["TandaLabor"].ToString(),
+                                PorcientoComision = DR["PorcientoComision"].ToString(),
+                                FechaIngreso = DR["FechaIngreso"].ToString(),
+                                Estado = Convert.ToBoolean(DR["Estado"]),
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lista = new List<Empleados>();
+            }
+            return lista;
+        }
+        public int Registrar(Empleados empleados, out string Mensaje)
+        {
+            int idAutoGenerado = 0;
+
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    //SqlCommand cmd = new SqlCommand("INSERT INTO Libros VALUES @Signa, @Nombre, @ISB, @BibliografiaId, @Autores, @Descripcion, @Ciencia, @Editora, @Estado, @Idioma, @year", oConexion);
+                    //cmd.Parameters
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("INSERT INTO Empleados ( Nombre, Cedula, TandaLabor, PorcientoComision, FechaIngreso, Estado) ");
+                    sb.Append("VALUES ( @Nombre, @Cedula, @TandaLabor, @PorcientoComision,@FechaIngreso, @Estado)");
+
+                    using (SqlCommand cmd = new SqlCommand(sb.ToString(), oConexion))
+                    {
+                        cmd.Parameters.AddWithValue("@Nombre", empleados.Nombre);
+                        cmd.Parameters.AddWithValue("@Cedula", empleados.Cedula);
+                        cmd.Parameters.AddWithValue("@TandaLabor", empleados.TandaLabor);
+                        cmd.Parameters.AddWithValue("@PorcientoComision", empleados.PorcientoComision);
+                                cmd.Parameters.AddWithValue("@FechaIngreso", empleados.FechaIngreso); 
+                        cmd.Parameters.AddWithValue("@Estado", empleados.Estado); 
+
+
+                        oConexion.Open();
+                        cmd.ExecuteNonQuery();
+                        oConexion.Close();
+
+                        idAutoGenerado = 1;
+                        Mensaje = "Registro insertado exitosamente";
+                    }
+
+                    //oConexion.Open();
+
+                    //cmd.ExecuteNonQuery();
+
+                    //idAutoGenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    //Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                idAutoGenerado = 0;
+
+                Mensaje = ex.Message;
+            }
+
+            return idAutoGenerado;
+        }
+        public bool Editar(Empleados empleados, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    //SqlCommand cmd = new SqlCommand("INSERT INTO Libros VALUES @Signa, @Nombre, @ISB, @BibliografiaId, @Autores, @Descripcion, @Ciencia, @Editora, @Estado, @Idioma, @year", oConexion);
+                    //cmd.Parameters
+
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("UPDATE Empleados SET ");
+                    sb.Append("Nombre = @Nombre,");
+                    sb.Append("Cedula = @Cedula,");
+                    sb.Append("TandaLabor = @TandaLabor,");
+                    sb.Append("PorcientoComision = @PorcientoComision,");
+                    sb.Append("FechaIngreso = @FechaIngreso,");
+                    sb.Append("Estado = @Estado ");
+                    sb.Append("WHERE Id = @Id");
+
+                    using (SqlCommand cmd = new SqlCommand(sb.ToString(), oConexion))
+                    {
+                        cmd.Parameters.AddWithValue("@Nombre", empleados.Nombre);
+                        cmd.Parameters.AddWithValue("@Cedula", empleados.Cedula);
+                        cmd.Parameters.AddWithValue("@TandaLabor", empleados.TandaLabor);
+                        cmd.Parameters.AddWithValue("@PorcientoComision", empleados.PorcientoComision);
+                        cmd.Parameters.AddWithValue("@FechaIngreso", empleados.FechaIngreso);
+                        cmd.Parameters.AddWithValue("@Estado", empleados.Estado);
+                        cmd.Parameters.AddWithValue("@Id", empleados.Id);
+
+                        oConexion.Open();
+                        cmd.ExecuteNonQuery();
+                        oConexion.Close();
+
+                        resultado = true;
+                    }
+                }
+                Mensaje = "Registro actualizado exitosamente";
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+
+            return resultado;
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("delete top (1) from Empleados where Id = @id", oConexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+                    // EXECUTE NON QUERY DEVUELVE EL TOTAL DE FILAS AFECTADAS
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+                Mensaje = "Registro eliminado exitosamente";
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
     }
 }
---
+++ CapaPresentacionApp/Scripts/Mantenimiento/Empleados.js
@@ -0,0 +1,237 @@
+﻿var tablaData;
+
+var filaSeleccionada;
+
+tablaData = $("#tablaLibros").DataTable
+    ({
+        responsive: true,
+        ordering: false,
+        "ajax": {
+            url: '/Mantenimiento/ListarEmpleados', /*@Url.Action("ListarEmpleados", "Mantenimiento")*/
+            type: "GET",
+            dataType: "json",
+        },
+        "columns":
+            [
+                { "data": "Nombre" },
+                { "data": "Cedula" },
+                { "data": "TandaLabor" },
+                { "data": "PorcientoComision" },
+                { "data": "FechaIngreso" },
+                {
+                    "data": "Estado", "render": function (valor) {
+                        if (valor == 1) {
+                            return '<span class="badge bg-success">Si</span>'
+                        } else {
+                            return '<span class="badge bg-danger">No</span>'
+                        }
+                    }
+                },
+                {
+                    "defaultContent": '<button type="button" class="btn btn-primary btn-sm btn-editar" ><i class="fas fa-pen me-1"></i></button>' +
+                        '<button type="button" class="btn btn-danger ms-2 btn-sm btn-eliminar"><i class="fas fa-trash me-1"></i></button>' +
+                        '<button type="button" class="btn btn-warning ms-2 btn-sm btn-info" > <i class= fas fa-info me-1" style="weight: bold;"></i> </button>',
+
+                    "orderable": false,
+                    "searchable": false,
+                    "width": "120px",
+                    "size": "20px"
+                }
+            ],
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        }
+
+    });
+
+
+function abrirModal(json) {
+
+    if (json != null) {
+        $("#mensajeError").hide()
+        $("#txtId").val(json.Id)
+        $("#txtNombre").val(json.Nombre)
+        $("#txtCedula").val(json.Cedula)
+        $("#txtTandaLabor").val(json.TandaLabor)
+        $("#txtPorcientoComision").val(json.PorcientoComision)
+        $("#txtFechaIngreso").val(json.FechaIngreso)
+        $("#cbxActivo").val(json.Estado == true ? 1 : 0)
+
+    } else {
+        $("#mensajeError").hide()
+        $("#txtId").val(0)
+        $("#txtNombre").val("")
+        $("#txtCedula").val("")
+        $("#txtTandaLabor").val("")
+        $("#txtPorcientoComision").val("")
+        $("#txtFechaIngreso").val("")
+        $("#cbxActivo").val("")
+    }
+
+    $("#FormModal").modal("show");
+}
+
+function Guardar() {
+
+    var Empleados = {
+        Id: $("#txtId").val(),
+        Nombre: $("#txtNombre").val(),
+        Cedula: $("#txtCedula").val(),
+        TandaLabor: $("#txtTandaLabor").val(),
+        PorcientoComision: $("#txtPorcientoComision").val(),
+        FechaIngreso: $("#txtFechaIngreso").val(),
+        Estado: $("#cbxActivo").val() == 1 ? true : false
+    }
+
+    jQuery.ajax({
+        url: '/Mantenimiento/GuardarEmpleados', /*@Url.Action("GuardarEmpleados", "Mantenimiento")*/
+        type: "POST",
+        data: JSON.stringify({ empleados: Empleados}), // parametro del metodo GuardarUsuario que es usuariox y se carga con el objeto Usuario
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) { //la data es lo que resivimos del url que viene del controlador metodo GuardarUsuario
+
+            //Empleados NUEVA
+            if (Empleados.Id == 0) { // si usuarioID = 0 es que se va a agregar
+
+                if (data.resultado != 0) {
+
+                    tablaData.ajax.reload()
+                    $("#FormModal").modal("hide");
+
+                    Swal.fire(data.mensaje , '', 'success')
+
+                } else {
+                    //error
+                    Swal.fire(
+                        '' + data.mensaje,
+                        '',
+                        'error'
+                    )
+                }
+            }
+            else { // editar
+
+                if (data.resultado) {
+
+                    tablaData.row(filaSeleccionada).data(Empleados).draw(false);
+                    filaSeleccionada = null;
+                    $("#FormModal").modal("hide");
+
+                    Swal.fire(data.mensaje, '', 'success')
+                } else {
+                    $("#mensajeError").text(data.mensaje);
+                    $("#mensajeError").show();
+                }
+            }
+        },
+        error: function (error) {
+            console.log(error)
+        },
+        beforeSend: function () {
+
+        }
+    });
+
+    $("#FormModal").modal("hide");
+}
+
+function Eliminar(json) {
+
+    var Empleados= {
+
+        IdEmpleados: json["Id"]
+
+    }
+
+    jQuery.ajax({
+        url: '/Mantenimiento/EliminarEmpleados', //@Url.Action("EliminarBibliografia", "Mantenimiento")
+        type: "POST",
+        data: JSON.stringify({ id: Empleados.IdEmpleados}), // parametro del metodo GuardarUsuario que es usuariox y se carga con el objeto Usuario
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) { //la data es lo que resivimos del url que viene del controlador metodo GuardarUsuario
+
+            if (data.resultado) {
+
+                tablaData.row(filaSeleccionada).draw(false);
+                filaSeleccionada = null;
+
+                Swal.fire(
+                    data.mensaje,
+                    '',
+                    'success'
+                )
+
+            } else {
+                $("#mensajeError").text(data.mensaje);
+
+                $("#mensajeError").show();
+                Swal.fire(
+                    data.mensaje,
+                    '',
+                    'error'
+                )
+            }
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    });
+
+    $("#FormModal").modal("hide");
+}
+
+function Detalles(json) {
+
+    console.log(json)
+
+    if (json != null) {
+        $("#mensajeError").hide()
+        $("#txtIdD").val(json.Id)
+        $("#txtNombreD").val(json.Nombre)
+        $("#txtCedulaD").val(json.Cedula)
+        $("#txtTandaLaborD").val(json.TandaLabor)
+        $("#txtPorcientoComisionD").val(json.PorcientoComision)
+        $("#txtFechaIngresoD").val(json.FechaIngreso)
+        $("#cbxActivoD").val(json.Estado == true ? 'Si' : 'No')
+
+        $("#ModalDetalles").modal("show");
+    }
+}
+////////////////////-----------------------------
+
+$("#tablaLibros tbody").on("click", '.btn-editar', function () {
+
+    filaSeleccionada = $(this).closest("tr")
+
+    var data = tablaData.row(filaSeleccionada).data()
+
+    abrirModal(data)
+    console.log(data.FechaIngreso)
+
+})
+
+$("#tablaLibros tbody").on("click", '.btn-eliminar', function () {
+
+    filaSeleccionada = $(this).closest("tr")
+
+    var data = tablaData.row(filaSeleccionada).data()
+
+    //console.log(tablaData.row(filaSeleccionada).data())
+
+    Eliminar(data)
+
+    tablaData.row(filaSeleccionada).remove();
+
+})
+
+$("#tablaLibros tbody").on("click", '.btn-info', function () {
+
+    filaSeleccionada = $(this).closest("tr")
+
+    var data = tablaData.row(filaSeleccionada).data()
+
+    Detalles(data)
+
+})
