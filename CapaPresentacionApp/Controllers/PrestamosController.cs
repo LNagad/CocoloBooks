fcce400666183b518aecc4e6f7c0794da0e47636
LNagad <nagadgames100k@gmail.com>
2022-04-24 15:20:27 -0400

    gestiones de prestamos en proceso

---
+++ CapaPresentacionApp/Controllers/PrestamosController.cs
@@ -0,0 +1,24 @@
+﻿using CapaEntidad;
+using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class PrestamosController : Controller
+    {
+        public JsonResult ListarUsuariosClientes()
+        {
+            List<Usuario> Olista = new List<Usuario>();
+
+            Olista = new CN_PrestamoLibros().ListarUsuariosClientes();
+
+            return Json(new { data = Olista }, JsonRequestBehavior.AllowGet );
+        }
+
+
+    }
+}
