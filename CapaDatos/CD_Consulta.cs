c0b463adcc4ad369d296dc7d5e49a1a538972c4b
AlanYoel <alanyoelsancheza@gmail.com>
2022-04-25 21:33:34 -0400

    Agregando el filtro

--- CapaDatos/CD_Consulta.cs
+++ CapaDatos/CD_Consulta.cs
@@ -67,7 +67,230 @@ namespace CapaDatos
             }
             return lista;
         }
+        public List<Libros> ListarPorCiencias(int id)
+        {
+            List<Libros> lista = new List<Libros>();
 
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = $"select * from vw_libros WHERE CienciaId = {id}";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader DR = cmd.ExecuteReader())
+                    {
+                        while (DR.Read())
+                        {
+                            lista.Add(
+
+                                new Libros()
+                                {
+                                    Id = Convert.ToInt32(DR["Id"]),
+                                    SignaturaTopografica = DR["SignaturaTopografica"].ToString(),
+                                    Nombre = DR["Nombre"].ToString(),
+                                    ISBN = Convert.ToDecimal(DR["ISBN"]),
+                                    Descripcion = DR["Descripcion"].ToString(),
+                                    BibliografiaId = Convert.ToInt32(DR["BibliografiaId"]), //fk
+                                    Bibliografia = DR["Bibliografia"].ToString(),
+                                    CienciaId = Convert.ToInt32(DR["CienciaId"]), // fk
+                                    Ciencia = DR["Ciencia"].ToString(),
+                                    AutorId = Convert.ToInt32(DR["AutorId"]), // fk
+                                    Autores = DR["Autor"].ToString(),
+                                    EditoraId = Convert.ToInt32(DR["EditoraId"]), // fk
+                                    Editora = DR["Editora"].ToString(),
+                                    IdiomaId = Convert.ToInt32(DR["IdiomaId"]), // fk
+                                    Idioma = DR["Idioma"].ToString(),
+                                    year = DR["year"].ToString(),
+                                    Estado = Convert.ToBoolean(DR["Estado"]),
+
+                                }
+
+                                ); ;
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lista = new List<Libros>();
+            }
+            return lista;
+        }
+        public List<Libros> ListarPorIdiomas(int id)
+        {
+            List<Libros> lista = new List<Libros>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = $"select * from vw_libros WHERE IdiomaId = {id}";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader DR = cmd.ExecuteReader())
+                    {
+                        while (DR.Read())
+                        {
+                            lista.Add(
+
+                                new Libros()
+                                {
+                                    Id = Convert.ToInt32(DR["Id"]),
+                                    SignaturaTopografica = DR["SignaturaTopografica"].ToString(),
+                                    Nombre = DR["Nombre"].ToString(),
+                                    ISBN = Convert.ToDecimal(DR["ISBN"]),
+                                    Descripcion = DR["Descripcion"].ToString(),
+                                    BibliografiaId = Convert.ToInt32(DR["BibliografiaId"]), //fk
+                                    Bibliografia = DR["Bibliografia"].ToString(),
+                                    CienciaId = Convert.ToInt32(DR["CienciaId"]), // fk
+                                    Ciencia = DR["Ciencia"].ToString(),
+                                    AutorId = Convert.ToInt32(DR["AutorId"]), // fk
+                                    Autores = DR["Autor"].ToString(),
+                                    EditoraId = Convert.ToInt32(DR["EditoraId"]), // fk
+                                    Editora = DR["Editora"].ToString(),
+                                    IdiomaId = Convert.ToInt32(DR["IdiomaId"]), // fk
+                                    Idioma = DR["Idioma"].ToString(),
+                                    year = DR["year"].ToString(),
+                                    Estado = Convert.ToBoolean(DR["Estado"]),
+
+                                }
+
+                                ); ;
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lista = new List<Libros>();
+            }
+            return lista;
+        }
+        public List<Libros> ListarPorAutores(int id)
+        {
+            List<Libros> lista = new List<Libros>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = $"select * from vw_libros WHERE AutorId = {id}";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader DR = cmd.ExecuteReader())
+                    {
+                        while (DR.Read())
+                        {
+                            lista.Add(
+
+                                new Libros()
+                                {
+                                    Id = Convert.ToInt32(DR["Id"]),
+                                    SignaturaTopografica = DR["SignaturaTopografica"].ToString(),
+                                    Nombre = DR["Nombre"].ToString(),
+                                    ISBN = Convert.ToDecimal(DR["ISBN"]),
+                                    Descripcion = DR["Descripcion"].ToString(),
+                                    BibliografiaId = Convert.ToInt32(DR["BibliografiaId"]), //fk
+                                    Bibliografia = DR["Bibliografia"].ToString(),
+                                    CienciaId = Convert.ToInt32(DR["CienciaId"]), // fk
+                                    Ciencia = DR["Ciencia"].ToString(),
+                                    AutorId = Convert.ToInt32(DR["AutorId"]), // fk
+                                    Autores = DR["Autor"].ToString(),
+                                    EditoraId = Convert.ToInt32(DR["EditoraId"]), // fk
+                                    Editora = DR["Editora"].ToString(),
+                                    IdiomaId = Convert.ToInt32(DR["IdiomaId"]), // fk
+                                    Idioma = DR["Idioma"].ToString(),
+                                    year = DR["year"].ToString(),
+                                    Estado = Convert.ToBoolean(DR["Estado"]),
+
+                                }
+
+                                ); ;
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lista = new List<Libros>();
+            }
+            return lista;
+        }
+        public List<Libros> ListarPorBibliografias(int id)
+        {
+            List<Libros> lista = new List<Libros>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = $"select * from vw_libros WHERE BibliografiaId = {id}";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader DR = cmd.ExecuteReader())
+                    {
+                        while (DR.Read())
+                        {
+                            lista.Add(
+
+                                new Libros()
+                                {
+                                    Id = Convert.ToInt32(DR["Id"]),
+                                    SignaturaTopografica = DR["SignaturaTopografica"].ToString(),
+                                    Nombre = DR["Nombre"].ToString(),
+                                    ISBN = Convert.ToDecimal(DR["ISBN"]),
+                                    Descripcion = DR["Descripcion"].ToString(),
+                                    BibliografiaId = Convert.ToInt32(DR["BibliografiaId"]), //fk
+                                    Bibliografia = DR["Bibliografia"].ToString(),
+                                    CienciaId = Convert.ToInt32(DR["CienciaId"]), // fk
+                                    Ciencia = DR["Ciencia"].ToString(),
+                                    AutorId = Convert.ToInt32(DR["AutorId"]), // fk
+                                    Autores = DR["Autor"].ToString(),
+                                    EditoraId = Convert.ToInt32(DR["EditoraId"]), // fk
+                                    Editora = DR["Editora"].ToString(),
+                                    IdiomaId = Convert.ToInt32(DR["IdiomaId"]), // fk
+                                    Idioma = DR["Idioma"].ToString(),
+                                    year = DR["year"].ToString(),
+                                    Estado = Convert.ToBoolean(DR["Estado"]),
+
+                                }
+
+                                ); ;
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                lista = new List<Libros>();
+            }
+            return lista;
+        }
 
     }
 }
